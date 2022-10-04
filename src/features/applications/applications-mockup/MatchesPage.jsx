aa0d3650142b31ffcc025c02419a46d56b181cc8
Michel <93651424+MJStarfield@users.noreply.github.com>
2022-10-04 10:47:52 +0200

    feat: Mockup Added

---
+++ src/features/applications/applications-mockup/MatchesPage.jsx
@@ -0,0 +1,21 @@
+import InfoCard from './InfoCard';
+import './MatchesPage.scss';
+
+const MatchesPage = () => {
+  return (
+    <div className="matches-container">
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+      <InfoCard></InfoCard>
+    </div>
+  );
+};
+
+export default MatchesPage;
