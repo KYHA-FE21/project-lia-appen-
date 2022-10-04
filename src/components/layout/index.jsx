46cf2d5acf289987a4b452a4c1e4d98a71d1cd6b
Michel <93651424+MJStarfield@users.noreply.github.com>
2022-10-04 10:41:37 +0200

    fix: badge class

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Badge from '../badge/badge';
 
 const Layout = () => {
   return (
     <>
       <header>Header</header>
+      <Badge>test</Badge>
       <main>
         <Outlet />
       </main>
