fbd3e560581a741f5b93eedb34429808e63b014c
Dennis Karlsson <tougent@gmail.com>
2022-10-04 10:36:16 +0200

    fix: components

--- src/features/profile/routes/index.jsx
+++ src/features/profile/routes/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Title from '../components/title';
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div style={{width: '100vw', height: '100vh', backgroundColor: 'red', margin: 0, padding: 0}}>
+      <Title>Göran Arvidsson</Title>
+    </div>
+  );
 };
 
 export default index;
