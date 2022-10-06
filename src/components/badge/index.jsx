4f7ab71103d6c080a840ea5e333a5f8c7de3fd5c
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-06 17:51:45 +0200

    fix: Badge component

---
+++ src/components/badge/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const Badge = ({ children, disabled = false, className = "" }) => {
+  const opacity = disabled ? 'opacity-5' : 'opacity-10';
+  return (
+    <div
+      className={`badge-container flex justify-center items-center flex-1 bg-primary ${opacity} ${className}`}
+    >
+      <span className="badge-text">{children}</span>
+    </div>
+  );
+};
+
+export default Badge;
