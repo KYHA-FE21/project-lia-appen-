d2ce794925e09684efddc214343bb821766fc47c
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-07 13:36:13 +0200

    fix(Badge): Remove flex-1

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -4,7 +4,7 @@ const Badge = ({ children, disabled = false, className = "" }) => {
   const opacity = disabled ? 'opacity-5' : 'opacity-10';
   return (
     <div
-      className={`badge-container flex justify-center items-center flex-1 bg-primary ${opacity} ${className}`}
+      className={`badge-container flex justify-center items-center bg-primary ${opacity} ${className}`}
     >
       <span className="badge-text">{children}</span>
     </div>
