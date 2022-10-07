e2d1457781725d4e8c3c41ffbbe2b7de603f9c14
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-10-07 15:26:56 +0200

    added back routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,9 @@ import Profile from './features/profile/routes';
 import Matchmake from './features/matchmake/routes';
 import Applications from './features/applications/routes';
 import Splash from './features/splash/routes';
+import Signin from './features/auth/routes/signin';
+import Signup from './features/auth/routes/signup';
+import Reset from './features/auth/routes/reset';
 
 const routes = () => {
   return (
@@ -13,6 +16,9 @@ const routes = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* <Route index element={<Splash />}></Route> */}
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/reset/:id" element={<Reset />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/matchmake" element={<Matchmake />}></Route>
           <Route path="/applications" element={<Applications />}></Route>
