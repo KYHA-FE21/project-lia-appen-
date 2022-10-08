1c86090dcc353760aafc19fa6f62514787f05360
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-08 18:42:57 +0200

    fix(PrimaryButton): Remove explicit font-size.

--- src/components/buttons/primary-button.jsx
+++ src/components/buttons/primary-button.jsx
@@ -1,32 +1,23 @@
 import "./primary-button.scss";
 
-const PrimaryButton = ({
-  width,
-  fontSize,
-  color = "primary",
-  logo,
-  onClick,
-  className = "",
-  children,
-}) => {
-  let colors = "";
-  if (color === "primary") colors = "text-white bg-primary";
-  else if (color === "white") colors = "text-black bg-white";
-  else if (color === "black") colors = "text-white bg-black";
-
-  return (
-    <button
-      className={`primary-button flex justify-center items-center gap-1 p-2 rounded-md ${colors} ${className}`}
-      style={{
-        width: width,
-        fontSize: fontSize,
-      }}
-      onClick={onClick}
-    >
-      {children && <span>{children}</span>}
-      {logo && logo}
-    </button>
-  );
-};
+function PrimaryButton({
+	width,
+	fontSize,
+	icon,
+	className = "",
+	children,
+	...restProps
+}) {
+	return (
+		<button
+			className={`primary-button p-2 px-4 flex justify-center items-center gap-1 rounded-md text-white bg-primary ${className}`}
+			style={{ width: width, fontSize: fontSize }}
+			{...restProps}
+		>
+			{children && <span>{children}</span>}
+			{icon}
+		</button>
+	);
+}
 
 export default PrimaryButton;
