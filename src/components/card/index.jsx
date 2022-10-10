8ad0e59cfa31b097f5b633b7670a5b51a2400a92
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-10 14:55:10 +0200

    fix(Card): Add flex class to top component.

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,7 +2,7 @@ import "./index.scss";
 
 function Card({ id, className = "", children }) {
 	return (
-		<article className={`shadow card-bg rounded-lg overflow-hidden ${className}`} id={id}>
+		<article className={`flex shadow card-bg rounded-lg overflow-hidden ${className}`} id={id}>
 			<div className="card-container gap-5 p-3 gradient">
 				{children}
 			</div>
