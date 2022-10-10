44be3fbf6174f2f47806e484cc7d130a3b225b93
Dennis Karlsson <tougent@gmail.com>
2022-10-10 10:21:47 +0200

    fix: update header

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../header/header';
 
 const Layout = () => {
   return (
     <>
-      <header>Header</header>
+      <Header />
       <main>
         <Outlet />
       </main>
