613cc877794f4530ae639d10bc8b857836c884e1
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-10 09:54:33 +0200

    fix(Badges): Padding & Allow props to be added.

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -1,14 +1,14 @@
 import './index.scss';
 
 const Badge = ({ children, disabled = false, className = "" }) => {
-  const opacity = disabled ? 'opacity-5' : 'opacity-10';
-  return (
-    <div
-      className={`badge-container flex justify-center items-center bg-primary ${opacity} ${className}`}
-    >
-      <span className="badge-text">{children}</span>
-    </div>
-  );
+	const opacity = disabled ? 'opacity-5' : 'opacity-10';
+	return (
+		<div
+			className={`badge-container flex px-2 justify-center items-center bg-primary ${opacity} ${className}`}
+		>
+			<span className="badge-text">{children}</span>
+		</div>
+	);
 };
 
 export default Badge;
