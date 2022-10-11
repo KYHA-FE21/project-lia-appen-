41a7fb26ed0b578b828acb7ce62b220e1df30e62
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-11 16:34:15 +0200

    wip: Add 2 column hero layout.

--- src/features/splash/components/sections/sales-pitch.jsx
+++ src/features/splash/components/sections/sales-pitch.jsx
@@ -95,10 +95,10 @@ function SalesPitchSection() {
 								<ProseParagraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vel doloremque rem rerum eveniet laudantium nulla? Magnam tenetur repellendus minus!</ProseParagraph>
 							</div>
 							<FlexContainer gap="3" direction="col" className="py-3">
-								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion" /> <label  className="flex-1" htmlFor="exampleQuestion">Svar 1</label></div>
-								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion2" /> <label className="flex-1"  htmlFor="exampleQuestion2">Svar 2</label></div>
-								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion3" /> <label className="flex-1"  htmlFor="exampleQuestion3">Svar 3</label></div>
-								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion4" /> <label className="flex-1"  htmlFor="exampleQuestion4">Svar 4</label></div>
+								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion" /> <label className="flex-1" htmlFor="exampleQuestion">Svar 1</label></div>
+								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion2" /> <label className="flex-1" htmlFor="exampleQuestion2">Svar 2</label></div>
+								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion3" /> <label className="flex-1" htmlFor="exampleQuestion3">Svar 3</label></div>
+								<div className="flex flex-1 gap-4 py-2"><input type="radio" name="exampleQuestion" id="exampleQuestion4" /> <label className="flex-1" htmlFor="exampleQuestion4">Svar 4</label></div>
 							</FlexContainer>
 							<CardButtons className="text-white">
 								<SecondaryButton className="flex-1" bgColor="" color="white" icon={<ArrowRight />}>Tillbaka</SecondaryButton>
--- src/features/splash/index.jsx
+++ src/features/splash/index.jsx
@@ -9,55 +9,52 @@ import SalesPitchSection from "./components/sections/sales-pitch";
 import Card, { CardHeader } from "../../components/card";
 
 function Splash() {
-  return (
-    <FlexContainer direction="col" gap="12" className="items-stretch">
-      <FlexContainer direction="col" gap="4" className="mx-8">
-        <div className="h-16 md:h-32 flex items-center justify-center">
-          <h1 className="text-3xl md:text-6xl text-center tracking-tighter">
-            <span>Hitta din </span>
-            <AnimatedText texts={["LIA plats...", "nästa kollega..."]} />
-          </h1>
-        </div>
-        <div>
-          <ProseParagraph textAlign="center" className="tracking">
-            Här kan du som student hitta en LIA-plats, och som företag hitta
-            dina framtida 🌟 <i>superstars</i>.
-            <br />
-            <strong>Helt gratis!</strong>
-          </ProseParagraph>
-        </div>
-      </FlexContainer>
+	return (
+		<FlexContainer direction="col" gap="12" className="items-stretch">
+			<FlexContainer gap="4" direction="col" desktopDirection="row" className="p-3 splash-cards-max-width-xl min-h-screen md:mx-auto">
+				<FlexContainer direction="col" className="flex-1 flex items-center justify-center gap-6">
+					<h1 className="h-16 md:h-32 text-3xl md:text-6xl tracking-tighter place-self-stretch">
+						<span>Hitta din </span>
+						<AnimatedText texts={["LIA plats...", "nästa kollega..."]} />
+					</h1>
 
-      <FlexContainer direction="col" className="mx-4 items-center">
-        <Card className="splash-cards-max-width-md">
-          <CardHeader className="text-2xl place-self-center">
-            <strong>Jag letar efter</strong>
-          </CardHeader>
-          <FlexContainer gap="4" className="items-stretch">
-            <LinkButton className="text-base p-4" href="#student">
-              Student
-            </LinkButton>
-            <LinkButton className="text-base p-4" href="#företag">
-              Företag
-            </LinkButton>
-          </FlexContainer>
-        </Card>
-      </FlexContainer>
+					<ProseParagraph className="my-6 tracking">
+						Här kan du som student hitta en LIA-plats, och som företag hitta
+						dina framtida 🌟 <i>superstars</i>.
+						<strong> Helt gratis!</strong>
+					</ProseParagraph>
 
-      <SalesPitchSection />
+					<Card className="w-full">
+						<CardHeader className="text-2xl place-self-center">
+							<strong>Jag letar efter</strong>
+						</CardHeader>
+						<FlexContainer gap="4" className="items-stretch">
+							<LinkButton className="text-base p-4" href="#student">
+								Student
+							</LinkButton>
+							<LinkButton className="text-base p-4" href="#företag">
+								Företag
+							</LinkButton>
+						</FlexContainer>
+					</Card>
+				</FlexContainer>
+				<FlexContainer className="flex-1"></FlexContainer>
+			</FlexContainer>
 
-      <FlexContainer direction="col" className="mx-4 items-center">
-        <Card className="splash-cards-max-width-xl">
-          <CardHeader className="place-self-center">
-            <strong>Vad väntar du på?</strong>
-          </CardHeader>
-          <LinkButton href="#" className="p-4 text-lg">
-            Registrera dig!
-          </LinkButton>
-        </Card>
-      </FlexContainer>
-    </FlexContainer>
-  );
+			<SalesPitchSection />
+
+			<FlexContainer direction="col" className="mx-4 items-center">
+				<Card className="splash-cards-max-width-xl">
+					<CardHeader className="place-self-center">
+						<strong>Vad väntar du på?</strong>
+					</CardHeader>
+					<LinkButton href="#" className="p-4 text-lg">
+						Registrera dig!
+					</LinkButton>
+				</Card>
+			</FlexContainer>
+		</FlexContainer>
+	);
 }
 
 export default Splash;
