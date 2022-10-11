42036b1962df50f6e306b8609aa5bddb496fe1c6
Daniel Schönning <dansch1982@gmail.com>
2022-10-11 14:56:57 +0200

    Creating Loading component

---
+++ src/features/matchmake/components/loading.jsx
@@ -0,0 +1,22 @@
+import "./loading.scss";
+
+function Loading() {
+	return (
+		<div class="lds-spinner m-auto w-full h-10">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+}
+
+export default Loading;
