65f502f38f52e1e9ca3324cb955aec2e19fef6f1
Michel Stjärnfelt <93651424+MJStarfield@users.noreply.github.com>
2022-10-11 13:45:10 +0200

    feat: Reusable global components & css class utilities (#24)
    Co-authored-by: Karar-Al <karar.alremahy@student.kyh.se>
    Co-authored-by: Daniel Schönning <dansch1982@gmail.com>
    Co-authored-by: Nik-Str <89907988+Nik-Str@users.noreply.github.com>

---
+++ src/components/buttons/index.jsx
@@ -0,0 +1,29 @@
+import "./primary-button.scss";
+
+function Button({
+	width,
+	fontSize,
+	icon,
+	color,
+	className = "",
+	children,
+	...restProps
+}) {
+	let colors = "";
+	if (color === "primary" || !color) colors = "text-white bg-primary";
+	else if (color === "white") colors = "text-black bg-white";
+	else if (color === "black") colors = "text-white bg-black";
+
+	return (
+		<button
+			className={`primary-button p-3 px-4 flex justify-center items-center gap-1 rounded-md ${colors} ${className}`}
+			style={{ width: width, fontSize: fontSize }}
+			{...restProps}
+		>
+			{children && <span>{children}</span>}
+			{icon}
+		</button>
+	);
+}
+
+export default Button;
