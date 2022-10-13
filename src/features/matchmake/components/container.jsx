c6da36b357aebc81ced5e37f46e583d54431a3e5
dansch1982 <84453251+dansch1982@users.noreply.github.com>
2022-10-13 13:33:57 +0200

    Feat: matchmake (#32)

---
+++ src/features/matchmake/components/container.jsx
@@ -0,0 +1,11 @@
+import { createElement, forwardRef } from "react";
+
+const Container = forwardRef(({ className, children, type, display, style: styleProp, id }, ref) => {
+	const style = {
+		display,
+		...styleProp,
+	};
+	return createElement(type || "div", { ref, id, className, style }, children);
+});
+
+export default Container;
