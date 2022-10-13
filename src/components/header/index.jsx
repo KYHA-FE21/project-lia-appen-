1544fce1644a8fe5523f1aa98620875dbb274834
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-10-13 15:45:23 +0200

    fixed logo color

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,21 @@
-import { Link } from 'react-router-dom';
-import { LogIn } from 'lucide-react';
-import './index.scss';
+import { Link } from "react-router-dom";
+import { LogIn } from "lucide-react";
+import "./index.scss";
 
 const Header = () => {
-  return (
-    <header>
-      <div className="header-container">
-        <Link to='/'><img className='logo' src='./logo.svg' alt='Shows the logotype'/></Link>
-  
-        <Link to="/login">
-          <LogIn size={24} color='white' />
-        </Link>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className="header-container">
+				<Link to="/">
+					<img className="logo" src="./logo.svg" alt="Shows the logotype" />
+				</Link>
+
+				<Link to="/login">
+					<LogIn size={24} color="white" />
+				</Link>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
