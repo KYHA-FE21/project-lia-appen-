0dfdfa15a5c9b789f6bdad0f24e6764385c3149b
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-10-13 10:52:23 +0200

    commit before merge

--- src/components/input-field/index.jsx
+++ src/components/input-field/index.jsx
@@ -1,49 +1,40 @@
 import { useEffect, useRef, useState } from "react";
 import "./index.scss";
 
-const InputField = ({
-  icon = null,
-  isError = false,
-  value,
-  handleChange,
-  ...restProps
-}) => {
-  const ref = useRef(null);
-  const [error, setError] = useState(null);
+const InputField = ({ icon = null, isError = false, value, handleChange, ...restProps }) => {
+	const ref = useRef(null);
+	const [error, setError] = useState(null);
 
-  const handleClick = () => {
-    ref.current.focus();
-  };
+	const handleClick = () => {
+		ref.current.focus();
+	};
 
-  useEffect(() => {
-    setError(isError);
-  }, [isError]);
+	useEffect(() => {
+		setError(isError);
+	}, [isError]);
 
-  let classes = []
-  if (error) classes.push('globalInputFieldError')
+	let classes = [];
+	if (error) classes.push("globalInputFieldError");
 
-  return (
-    <div
-      className="globalInputContainer"
-      onClick={handleClick}
-    >
-      <div className="globalInputContent">
-        {icon && <span className="globalInputIcon">{icon}</span>}
-        <input
-          ref={ref}
-          className={`globalInputContent globalInputField ${classes}`}
-          value={value}
-          onChange={handleChange}
-          {...restProps}
-        />
-      </div>
-      {error && <p className="globalInputError">{error}</p>}
-    </div>
-  );
+	return (
+		<div className="globalInputContainer" onClick={handleClick}>
+			<div className="globalInputContent">
+				{icon && <span className="globalInputIcon">{icon}</span>}
+				<input
+					ref={ref}
+					className={` globalInputField ${classes}`}
+					value={value}
+					onChange={handleChange}
+					{...restProps}
+				/>
+			</div>
+			{error && <p className="globalInputError">{error}</p>}
+		</div>
+	);
 };
 
-export function InputLabel ({children, ...restProps}) {
-  return <label {...restProps}>{children}</label>
+export function InputLabel({ children, ...restProps }) {
+	return <label {...restProps}>{children}</label>;
 }
 
 export default InputField;
--- src/features/auth/routes/signin.jsx
+++ src/features/auth/routes/signin.jsx
@@ -1,62 +1,62 @@
-import React from 'react';
-import '../styles/auth.scss';
-import { Mail, Lock } from 'lucide-react';
-import TextField from '../components/textField';
-import Logo from '../components/logo';
-import Path from '../components/path';
-import Btn from '../components/btn';
-import External from '../components/external';
-import usePOST from '../hooks/usePost';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "../styles/auth.scss";
+import { Mail, Lock } from "lucide-react";
+import TextField from "../components/textField";
+import Logo from "../components/logo";
+import Path from "../components/path";
+import Btn from "../components/btn";
+import External from "../components/external";
+import usePOST from "../hooks/usePost";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  const { data, loading, error, fetchPost } = usePOST;
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+	const { data, loading, error, fetchPost } = usePOST;
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    fetchPost('/api/user/signin', { email, password });
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		fetchPost("/api/user/signin", { email, password });
+	};
 
-  const navigate = useNavigate();
-  React.useEffect(() => {
-    if (data) navigate('/profile');
-  }, [data]);
+	const navigate = useNavigate();
+	React.useEffect(() => {
+		if (data) navigate("/profile");
+	}, [data]);
 
-  return (
-    <div className="authContainer">
-      <div className="authContent">
-        <Logo />
-        <form onSubmit={handleSubmit}>
-          <TextField
-            icon={<Mail strokeWidth={1} />}
-            type="email"
-            placeholder="E-post"
-            value={email}
-            setValue={setEmail}
-            isError={error?.type === 'email' && error.message}
-          />
-          <TextField
-            icon={<Lock strokeWidth={1} />}
-            type="password"
-            placeholder="Lösenord"
-            value={password}
-            setValue={setPassword}
-            isError={error?.type === 'password' && error.message}
-          />
-          <Btn title="LOGGA IN" loading={loading} />
-        </form>
-        <Path
-          links={[
-            { path: '/signup', title: 'Skapa Konto' },
-            { path: '/reset/*', title: 'Glöm Lösenord?' },
-          ]}
-        />
-        <External />
-      </div>
-    </div>
-  );
+	return (
+		<div className="authContainer">
+			<div className="authContent">
+				<Logo />
+				<form onSubmit={handleSubmit}>
+					<TextField
+						icon={<Mail strokeWidth={1} />}
+						type="email"
+						placeholder="E-post"
+						value={email}
+						setValue={setEmail}
+						isError={error?.type === "email" && error.message}
+					/>
+					<TextField
+						icon={<Lock strokeWidth={1} />}
+						type="password"
+						placeholder="Lösenord"
+						value={password}
+						setValue={setPassword}
+						isError={error?.type === "password" && error.message}
+					/>
+					<Btn title="LOGGA IN" loading={loading} />
+				</form>
+				<Path
+					links={[
+						{ path: "/signup", title: "Skapa Konto" },
+						{ path: "/reset/*", title: "Glöm Lösenord?" },
+					]}
+				/>
+				<External />
+			</div>
+		</div>
+	);
 };
 
 export default Signin;
