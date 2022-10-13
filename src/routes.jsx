a764c585b7664d306adb5af5f5b85ea2a187af5c
Niklas Strömberg <niklas-stromberg@live.se>
2022-10-13 15:23:05 +0200

    Feature/questionnaire (#40)
    * init questionnarie routes
    
    * added basic layout create and overview questions
    
    * fix: Re-add routes.
    
    * Update src/features/questionnaire/styles/quest.scss
    
    Co-authored-by: Karar-Al <95925135+Karar-Al@users.noreply.github.com>
    
    Co-authored-by: Karar-Al <karar.alremahy@student.kyh.se>
    Co-authored-by: Karar-Al <95925135+Karar-Al@users.noreply.github.com>

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,17 +9,27 @@ import Splash from "./features/splash/routes";
 import Signin from "./features/auth/routes/signin";
 import Signup from "./features/auth/routes/signup";
 import Reset from "./features/auth/routes/reset";
+import CreateQuestionnaire from "./features/questionnaire/routes/create";
+import OverviewQuestionnaire from "./features/questionnaire/routes/overview";
 
 const routes = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					{/* <Route index element={<Splash />}></Route> */}
-					{/* <Route path="/login" element={<Login />}></Route> */}
+					<Route index element={<Splash />}></Route>
+					<Route path="/login" element={<Signin />}></Route>
 					<Route path="/profile" element={<Profile />}></Route>
 					<Route path="/matchmake" element={<Matchmake />}></Route>
 					<Route path="/applications" element={<Applications />}></Route>
+					<Route
+						path="/questionnaire/overview/:id"
+						element={<OverviewQuestionnaire />}
+					></Route>
+					<Route
+						path="/questionnaire/create/:id"
+						element={<CreateQuestionnaire />}
+					></Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
