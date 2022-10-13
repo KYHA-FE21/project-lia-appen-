eb77cbea09a1b046460c4d21d8ae9410f2e72473
Dennis Karlsson <30557440+Ghilea@users.noreply.github.com>
2022-10-13 13:38:56 +0200

    fix: update header (#30)
    Co-authored-by: Karar-Al <95925135+Karar-Al@users.noreply.github.com>

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from '../header/header';
 import Footer from '../footer/footer';
 
 const Layout = () => {
 	return (
 		<>
-			<header>Header</header>
+			<Header />
 			<main>
 				<Outlet />
 			</main>
