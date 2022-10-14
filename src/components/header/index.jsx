a09fbc638c459e9332a3ffb90706459d4f815412
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-14 10:40:17 +0200

    fix: Add padding to header. Additionally changed names of max-content utilities.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,13 +4,13 @@ import "./index.scss";
 
 const Header = () => {
 	return (
-		<header>
+		<header className="main-header h-max">
 			<div className="header-container">
-				<Link to="/">
+				<Link to="/" className="p-3">
 					<img className="logo" src="./logo.svg" alt="Shows the logotype" />
 				</Link>
 
-				<Link to="/login">
+				<Link to="/signin" title="Signin" className="h-full px-4">
 					<LogIn size={24} color="white" />
 				</Link>
 			</div>
