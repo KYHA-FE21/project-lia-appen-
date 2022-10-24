85523c935ff17cec6b7c7a1d066ec1eba616a152
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-24 13:04:23 +0200

    fix: add white bg to top splash card

--- src/features/splash/index.jsx
+++ src/features/splash/index.jsx
@@ -22,7 +22,7 @@ function Splash() {
 				>
 					<FlexContainer
 						direction="col"
-						className="flex-1 flex items-center justify-center gap-6 rounded-md p-3 md:p-16"
+						className="flex-1 splash-card-bg-white p-16 flex items-center justify-center gap-6 rounded-md p-3 md:p-16"
 					>
 						<h1 className="splash-mb-2 h-16 text-3xl md:text-6xl tracking-tighter">
 							<span>Hitta din </span>
