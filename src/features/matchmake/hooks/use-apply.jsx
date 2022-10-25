a559b6fc72d6dfa88e395732cbe3c42fea45ff59
Daniel Schönning <dansch1982@gmail.com>
2022-10-25 10:56:27 +0200

    Creating useapply hook

---
+++ src/features/matchmake/hooks/use-apply.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import postApplicant from "../api/post-applicant";
+
+function useApply() {
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [applied, setApplied] = useState(false);
+
+	async function apply(advertisementData, user, bool) {
+		setLoading(true);
+		setError(null);
+		setTimeout(async () => {
+			try {
+				const { advertisement } = advertisementData;
+				const date = Intl.DateTimeFormat("sv-SE").format(new Date());
+				const body = JSON.stringify({
+					advertisement_id: advertisement.id,
+					user_id: user.id,
+					accepted: bool === true ? !bool : null,
+					date,
+				});
+				await postApplicant(body);
+				setApplied(bool);
+			} catch (error) {
+				setError(error.toString());
+			} finally {
+				setLoading(false);
+			}
+		}, 1_000);
+	}
+
+	return { loading, error, applied, apply };
+}
+
+export default useApply;
