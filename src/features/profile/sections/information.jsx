246eca2e3156f71d226dd06a690aa417eb41fc63
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-31 14:30:12 +0100

    fix: Format

--- src/features/profile/sections/information.jsx
+++ src/features/profile/sections/information.jsx
@@ -1,63 +1,59 @@
-import React from 'react'
-import Wrapper from '../components/wrapper';
-import Title from '../components/title';
-import { Phone, MapPin, CalendarDays } from 'lucide-react';
+import React from "react";
+import Wrapper from "../components/wrapper";
+import Title from "../components/title";
+import { Phone, MapPin, CalendarDays } from "lucide-react";
 
-const Information = ({ name, profession, school, date, bio, location, phone }) => {
+const Information = ({
+	name,
+	profession,
+	school,
+	date,
+	bio,
+	location,
+	phone,
+}) => {
+	return (
+		<Wrapper
+			padding={[5, 0, 0, 0]}
+			direction="column"
+			gap={[5]}
+			styleDirection="center"
+		>
+			<Title size={[1.5]} bold={700} subTitle={profession}>
+				{name}
+			</Title>
 
-    return (
-        <Wrapper 
-            padding={[5, 0, 0, 0]} 
-            direction='column' 
-            gap={[5]} 
-            styleDirection='center'>
-            <Title 
-                size={[1.5]} 
-                bold={700} 
-                subTitle={profession}>{name}</Title>
-            
-            <Wrapper 
-                wrap='wrap' 
-                gap={[1]} 
-                styleDirection='center'>
+			<Wrapper wrap="wrap" gap={[1]} styleDirection="center">
+				{location === undefined && (
+					<Title size={[1]} img={<MapPin color="black" size={18} />}>
+						{location}
+					</Title>
+				)}
 
-                {location === undefined ||
-                    <Title 
-                        size={[1]} 
-                        img={<MapPin color='black' size={18} />}>{location}</Title>
-                }
+				{phone === undefined && (
+					<Title size={[1]} img={<Phone color="black" size={18} />}>
+						{phone}
+					</Title>
+				)}
 
-                {phone === undefined ||
-                    <Title 
-                        size={[1]}
-                        img={<Phone color='black' size={18} />}>{phone}</Title>
-                }
-                
-                {school === undefined ||
-                    <Title 
-                        size={[1]} 
-                        img={<MapPin color='black' size={18} />}>{school}</Title>
-                }
+				{school === undefined && (
+					<Title size={[1]} img={<MapPin color="black" size={18} />}>
+						{school}
+					</Title>
+				)}
 
-                {date === undefined ||
-                    <Title 
-                        size={[1]}
-                        img={<CalendarDays color='black' size={18} />}>{date}</Title>
-                }  
-                
-            </Wrapper>
+				{date === undefined && (
+					<Title size={[1]} img={<CalendarDays color="black" size={18} />}>
+						{date}
+					</Title>
+				)}
+			</Wrapper>
 
-            <Wrapper 
-                maxWidth='800px' 
-                lineHeight={[1.5]} 
-                padding={[0, 1]}>
-                
-                <Title 
-                    size={[1]}>{bio}</Title>
-            </Wrapper>
+			<Wrapper maxWidth="800px" lineHeight={[1.5]} padding={[0, 1]}>
+				<Title size={[1]}>{bio}</Title>
+			</Wrapper>
+		</Wrapper>
+	);
+};
 
-        </Wrapper>
-    )
-}
-
-export default Information
+export default Information;
