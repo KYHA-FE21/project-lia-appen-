7ac22c6e00eb1e055b1d9ec90d4a2906dfe5284f
Karar-Al <karar.alremahy@student.kyh.se>
2022-10-31 15:37:50 +0100

    fix: Move hooks to separate files.

---
+++ src/features/profile/hooks/use-user.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { getAttributeByID, putAttributesByID } from "../api/attribute";
+import { getUserByID, putUserByID } from "../api/user";
+
+const defaultUser = {
+	attribute: {
+		badges: [],
+	},
+};
+
+export default function useUser({ id }) {
+	const [data, setData] = useState(defaultUser);
+	const [lastUpdate, setLastUpdate] = useState(Date.now());
+
+	useEffect(() => {
+		let cancelled = false;
+
+		async function getUserAndAttributes() {
+			const user = await getUserByID(id);
+
+			if (!user.data) return;
+
+			const attribute = await getAttributeByID(user.data.attribute_id);
+
+			if (!cancelled) {
+				setData({ ...user.data, attribute: attribute.data });
+			}
+		}
+
+		getUserAndAttributes();
+
+		return () => {
+			cancelled = true;
+		};
+	}, [id, lastUpdate]);
+
+	async function update(data) {
+		const user = { ...data };
+		const attribute = data.attribute;
+
+		delete user.attribute;
+
+		const [userResp, attributeResp] = await Promise.all([
+			putUserByID(user.id, JSON.stringify(user)),
+			putAttributesByID(attribute.id, JSON.stringify(attribute)),
+		]);
+
+		setLastUpdate(Date.now());
+
+		return { ...userResp.data, attribute: attributeResp.data };
+	}
+
+	return {
+		data,
+		update,
+	};
+}
