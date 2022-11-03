1556e2449f53830436afe912c7b8f0c9f2e1666c
Daniel Schönning <dansch1982@gmail.com>
2022-11-03 09:48:44 +0100

    refactor getuser

--- src/features/applications/api/get-user.jsx
+++ src/features/applications/api/get-user.jsx
@@ -7,9 +7,9 @@ function getUser(searchParams = []) {
 	const API_URL = process.env.REACT_APP_BACKEND_ENDPOINT;
 	const API_ENDPOINT = "user";
 	const url = new URL(API_ENDPOINT, API_URL);
-	searchParams.forEach((value, key) => {
+	for (const [key, value] of searchParams) {
 		url.searchParams.append(key, value);
-	});
+	}
 	return fetch(url);
 }
 
