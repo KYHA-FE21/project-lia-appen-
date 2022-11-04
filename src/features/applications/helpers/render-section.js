6cd33fbce9c5736a721da8434f297eb92dacc551
Daniel Schönning <dansch1982@gmail.com>
2022-11-04 14:21:47 +0100

    style fix for titles

--- src/features/applications/helpers/render-section.js
+++ src/features/applications/helpers/render-section.js
@@ -1,7 +1,7 @@
 function renderSection(title, array, component) {
 	return (
 		<>
-			<h1>
+			<h1 className="text-xl text-center">
 				{title} - {array.length}
 			</h1>
 			<div className="flex flex-wrap gap-3 justify-center">{array.map(component)}</div>
