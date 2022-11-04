24c276a29b1fd3bfd3e527620e042236099c4c24
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-11-04 13:45:48 +0100

    removed unresolved changes

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -1,10 +1,7 @@
-const Badge = ({ children, disabled = false, className = "", ...restProps }) => {
+const Badge = ({ children, disabled = false, className = "" }) => {
 	const opacity = disabled ? "opacity-5" : "opacity-10";
 	return (
-		<div
-			className={`flex justify-center items-center text-tiny rounded-pill py-1 shadow px-2 bg-primary ${opacity} ${className}`}
-			{...restProps}
-		>
+		<div className={`flex justify-center items-center text-tiny rounded-pill py-1 shadow px-2 bg-primary ${opacity} ${className}`}>
 			<span className="mx-1">{children}</span>
 		</div>
 	);
