141bf9f66f10c505c4d07df76eb07d3b20f7efa7
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-11-04 14:28:48 +0100

    removed card to separate components

---
+++ src/features/advertisement/components/advCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Card, { CardHeader, CardBadges, CardButtons } from "../../../components/card";
+import InfoStats from "../../../components/info-stats/info-stats";
+import Badges from "../../../components/badge";
+import InfoGrid from "../../../components/info-grid";
+import { CalendarDays, MapPin, CheckCircle } from "lucide-react";
+
+const AdvCard = ({ add, children }) => {
+	return (
+		<Card>
+			<CardHeader className="text-2xl text-white px-3">
+				<h2 className="">{add.attribute.profession}</h2>
+			</CardHeader>
+			<InfoGrid
+				className="px-3"
+				color="white"
+				entries={[
+					{
+						icon: <CalendarDays size="20" />,
+						children: (
+							<span className="text-tiny">
+								{add.attribute.period[0]} <br /> {add.attribute.period[1]}
+							</span>
+						),
+					},
+					{
+						icon: <MapPin size="20" />,
+						children: <span className="text-tiny">{add.attribute.location}</span>,
+					},
+					{
+						icon: <CheckCircle size="20" />,
+						children: <span className="text-tiny">{add.attribute.work_type}</span>,
+					},
+				]}
+			/>
+			<div className="divider" />
+			<CardBadges>
+				{add.attribute.badges.map((badge, i) => (
+					<Badges key={badge + i} className="text-white">
+						{badge}
+					</Badges>
+				))}
+			</CardBadges>
+			<div className="divider" />
+			<InfoStats
+				statistics={{
+					response_time: add.attribute.response_time,
+					openings: add.attribute.openings,
+					decline_rate: add.attribute.decline_rate,
+				}}
+			/>
+			<div className="divider" />
+			<CardButtons>{children}</CardButtons>
+		</Card>
+	);
+};
+
+export default AdvCard;
--- src/features/advertisement/routes/index.jsx
+++ src/features/advertisement/routes/index.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "../styles/index.scss";
-import Button from "../../../components/buttons";
 import Modal from "../components/modal";
 import useAdvertisementController from "../api/adv-controller";
-import Card, { CardHeader, CardBadges, CardButtons } from "../../../components/card";
-import InfoStats from "../../../components/info-stats/info-stats";
-import Badges from "../../../components/badge";
-import InfoGrid from "../../../components/info-grid";
-import { CalendarDays, MapPin, CheckCircle } from "lucide-react";
 import AuthContext from "../../../context";
+import AdvCard from "../components/advCard";
+import Button from "../../../components/buttons";
 
 const Advertisement = () => {
 	const { data, loading, error, getAdvertisements, postAdvertisement, patchAttributes, removeAdvertisements } =
@@ -52,61 +48,17 @@ const Advertisement = () => {
 						{!error && (
 							<div className="flex flex-wrap justify-center gap-6">
 								{data.map((add) => (
-									<Card key={add.id}>
-										<CardHeader className="text-2xl text-white px-3">
-											<h2 className="">{add.attribute.profession}</h2>
-										</CardHeader>
-										<InfoGrid
-											className="px-3"
-											color="white"
-											entries={[
-												{
-													icon: <CalendarDays size="20" />,
-													children: (
-														<span className="text-tiny">
-															{add.attribute.period[0]} <br /> {add.attribute.period[1]}
-														</span>
-													),
-												},
-												{
-													icon: <MapPin size="20" />,
-													children: <span className="text-tiny">{add.attribute.location}</span>,
-												},
-												{
-													icon: <CheckCircle size="20" />,
-													children: <span className="text-tiny">{add.attribute.work_type}</span>,
-												},
-											]}
-										/>
-										<div className="divider" />
-										<CardBadges>
-											{add.attribute.badges.map((badge, i) => (
-												<Badges key={badge + i} className="text-white">
-													{badge}
-												</Badges>
-											))}
-										</CardBadges>
-										<div className="divider" />
-										<InfoStats
-											statistics={{
-												response_time: add.attribute.response_time,
-												openings: add.attribute.openings,
-												decline_rate: add.attribute.decline_rate,
+									<AdvCard key={add.id}>
+										<Button
+											className="w-full"
+											onClick={() => {
+												setPatchData(add);
+												setModalDisplay(true);
 											}}
-										/>
-										<div className="divider" />
-										<CardButtons>
-											<Button
-												className="w-full"
-												onClick={() => {
-													setPatchData(add);
-													setModalDisplay(true);
-												}}
-											>
-												Editera
-											</Button>
-										</CardButtons>
-									</Card>
+										>
+											Editera
+										</Button>
+									</AdvCard>
 								))}
 							</div>
 						)}
