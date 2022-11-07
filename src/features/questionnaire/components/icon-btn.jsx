af24d7be4bcf8e9cbc8d38f2b947518538035f7b
Karar-Al <karar.alremahy@student.kyh.se>
2022-11-07 14:53:54 +0100

    fix: Remove color CSS rule and add class instead.
    Co-authored-by: Nik-Str <89907988+Nik-Str@users.noreply.github.com>

--- src/features/questionnaire/components/icon-btn.jsx
+++ src/features/questionnaire/components/icon-btn.jsx
@@ -1,7 +1,7 @@
 const IconBtn = ({ icon, onClick, disabled }) => {
 	return (
 		<button
-			className="questIconBtn shadow bg-primary mx-2 p-2 rounded-md"
+			className="questIconBtn text-white shadow bg-primary mx-2 p-2 rounded-md"
 			type="button"
 			onClick={onClick}
 			disabled={disabled}
