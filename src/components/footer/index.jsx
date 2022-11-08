7663cbd6b21b05ba68e146ea6ca29aa21b3ed43e
Karar-Al <karar.alremahy@student.kyh.se>
2022-11-08 14:10:44 +0100

    fix: Footer & Header links now use auth context

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,31 +1,37 @@
 import { Link } from "react-router-dom";
 import "./index.scss";
 
-import { Contact, HeartHandshake, Settings } from "lucide-react";
+import { Contact, HeartHandshake, Loader } from "lucide-react";
 
-const Footer = () => {
+const Footer = ({ user }) => {
 	return (
 		<footer className="main-footer bg-primary text-white">
 			<div className="footer-container max-w-screen-lg mx-auto">
-				{/**
-				 * TODO: Link differently based on currently logged in user type.
-				 * type === "company" = "/advertisements",
-				 * type === "student" = "/applications"
-				 */}
-				<Link to="/applications" className="p-4 flex-1">
-					<HeartHandshake />
-				</Link>
-				{/**
-				 * TODO: Link differently based on currently logged in user type.
-				 * type === "company" = "/profiles",
-				 * type === "student" = "/matchmake"
-				 */}
-				<Link to="/matchmake" className="p-4 flex-1">
-					<Contact />
-				</Link>
-				<Link to="/profile" className="p-4 flex-1">
-					<Settings />
-				</Link>
+				{user?.loading && (
+					<div className="p-4 flex-1 flex items-center justify-center">
+						<Loader />
+					</div>
+				)}
+
+				{user?.data && !user.loading && (
+					<>
+						{user.data.attribute.type === "student" ? (
+							<Link to="/applications" className="p-4 flex-1">
+								<HeartHandshake />
+							</Link>
+						) : (
+							<Link to="/advertisement" className="p-4 flex-1">
+								<HeartHandshake />
+							</Link>
+						)}
+
+						{user.data.attribute.type === "student" && (
+							<Link to="/matchmake" className="p-4 flex-1">
+								<Contact />
+							</Link>
+						)}
+					</>
+				)}
 			</div>
 		</footer>
 	);
