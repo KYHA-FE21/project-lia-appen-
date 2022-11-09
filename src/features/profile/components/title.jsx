560f108fb64f862b1ac39991b71f8b0e7663c870
Karar-Al <95925135+Karar-Al@users.noreply.github.com>
2022-11-09 14:37:56 +0100

    fix(Profile): Flip logic for information grid. (#150)

--- src/features/profile/components/title.jsx
+++ src/features/profile/components/title.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
-const Title = ({children, img, size, bold, subTitle}) => {
-  
-  const [styleSize, setStyleSize] = useState();
-  const [styleBold, setStyleBold] = useState();
+const Title = ({ children, img, size, bold, subTitle, className = "" }) => {
+	const [styleSize, setStyleSize] = useState();
+	const [styleBold, setStyleBold] = useState();
 
-  useEffect(() => {
-    if (size !== undefined) setStyleSize(size.join('rem ') + 'rem')
-  }, [size])
+	useEffect(() => {
+		if (size !== undefined) setStyleSize(size.join("rem ") + "rem");
+	}, [size]);
 
-  useEffect(() => {
-    if (bold !== undefined) setStyleBold(bold)
-  }, [bold])
-  
-  return (
-    <h2 className={`${(img !== undefined) ? 'title' : '' }`} 
-    style={{
-      fontSize: styleSize || 1 + 'rem',
-      fontWeight: styleBold || 400
-    }}><span>{img}</span>{children}<br /><span className='subTitle'>{subTitle}</span>
-    </h2>
-  )
-}
+	useEffect(() => {
+		if (bold !== undefined) setStyleBold(bold);
+	}, [bold]);
+
+	return (
+		<h2
+			className={`${img !== undefined ? "title" : ""} ${className}`}
+			style={{
+				fontSize: styleSize || 1 + "rem",
+				fontWeight: styleBold || 400,
+			}}
+		>
+			{img}
+			<span className="flex items-center justify-center">{children}</span>
+			{subTitle && (
+				<>
+					<br />
+					<span className="subTitle">{subTitle}</span>
+				</>
+			)}
+		</h2>
+	);
+};
 
 export default Title;
